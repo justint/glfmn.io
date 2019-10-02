1dfefe31d2dd7d7ead73ced37db5ea4daa49d553
Gwen Lofman <Gwen@Lofman.io>
2019-10-01 23:37:33 -0400

    Initial commit from gatsby
    Used Gatsby starter kit but changed file extensions where necesary
    and updated project to use scss.

---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gwen Lofman`,
+    description: `A tech blog written by Gwen Lofman`,
+    author: `@glfmn`,
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `glfmn-io`,
+        short_name: `glfmn`,
+        start_url: `/`,
+        background_color: `#fbf1c7`,
+        theme_color: `#282828`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/components/layout.jsx
@@ -0,0 +1,40 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import "./layout.scss"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <div className='page-container'>
+        <main>{children}</main>
+        <footer>© {new Date().getFullYear()}</footer>
+      </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
