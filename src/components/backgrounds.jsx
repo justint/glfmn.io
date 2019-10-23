47ae2c65b817d47b79962a87e1433d9bfcf62d27
Gwen Lofman <Gwen@Lofman.io>
2019-10-23 15:28:55 -0400

    Create loadable wrapper around ROT Display
    Wrapper includes a small text spinner which will be displayed
    instead of the background if it takes a long time to load.  It
    imports the rot default export (which is the display) and renders
    it forwarding all props.

--- src/components/backgrounds.jsx
+++ src/components/backgrounds.jsx
@@ -1,38 +1,102 @@
-import React, { useContext } from 'react'
-import Rot from './rot'
+import React, { useContext, useRef, useState, useEffect } from 'react'
 import { ResizeContext } from '../resize'
 
-export const Background = ({ bg }) => {
+import Loadable from 'react-loadable'
+
+const Display = Loadable({
+  loader: () => import('./rot'),
+  loading: () => <Spinner type='circle' />,
+  render: (loaded, props) => {
+    const Component = loaded.default
+    return <Component {...props} />
+  }
+})
+
+const Spinner = ({ type, items, rate, pastDelay }) => {
+  const spinners = {
+    'dots': ['⠁', '⠂', '⠄', '⡀', '⢀', '⠠', '⠐', '⠈'],
+    'bar': ['/', '|', '\\', '-'],
+    'elipsis': ['   ', '.  ', '.. ', '...', ' ..', '  .'],
+    'elipsis bar': ['⋰', '⋮', '⋱', '⋯'],
+    'circle': ['◜', '◠', '◝', '◞', '◡', '◟'],
+  }
+  const selection = type in spinners ? spinners[type] : spinners['dots']
+  const spin = items ? items : selection
+  const [index, setIndex] = useState(0)
+  const inc = () => setIndex((index + 1) % spin.length)
+  useInterval(inc, rate || 250)
+  if (pastDelay)
+    return <pre style={{ padding: 0, margin: 0 }}>{spin[index]}</pre>
+  else
+    return <></>
+}
+
+const Background = ({ bg }) => {
   let rect = useContext(ResizeContext)
 
   const width = rect ? rect.width : 720
   const height = rect ? rect.height : 480
 
-  const arrows = (display) => {
-    display.clear()
-
-    const { height, width } = display.getOptions()
-    if (width < 80) {
-      for (let x = 0; x < width; x += 5) {
-        display.draw(x - 1, 0, '─')
-        display.draw(x, 0, '▬')
-        display.draw(x + 1, 0, '─')
-        display.draw(x, 1, '⁘')
-      }
-    } else {
-      for (let y = 0; y < height; y += 5) {
-        display.draw(3, y - 1, '│')
-        display.draw(3, y, '❚')
-        display.draw(3, y + 1, '│')
-        display.draw(5, y, '⁘')
-      }
-    }
-  }
-
   switch (bg) {
+    case 'noise':
+      return <Display width={width} height={height} draw={noise} interval={66} />
     default:
-      return <Rot width={width} height={height} draw={arrows} />
+      return <Display width={width} height={height} draw={arrows} />
   }
 }
 
 export default Background
+
+const noise = (display) => {
+  if (!display) return
+
+  display.clear()
+  const { height, width } = display.getOptions()
+
+  for (let y = 0; y < height; y++)
+    for (let x = 0; x < width; x++)
+      if (Math.random() > 0.9)
+        display.draw(Math.floor(width * Math.random()), y, '.')
+}
+
+const arrows = (display) => {
+  if (!display)
+    return
+
+  display.clear()
+  const { height, width } = display.getOptions()
+
+  if (width < 1.5 * height) {
+    for (let x = 4; x < width; x += 5) {
+      display.draw(x - 4, 0, '─')
+      display.draw(x - 3, 0, '▬')
+      display.draw(x - 2, 0, '─')
+      display.draw(x - 3, 1, '⁘')
+    }
+  } else {
+    for (let y = 8; y < height; y += 5) {
+      display.draw(3, y - 4, '│')
+      display.draw(3, y - 3, '❚')
+      display.draw(3, y - 2, '│')
+      display.draw(5, y - 3, '⁘')
+    }
+  }
+}
+
+function useInterval(fn, delay, ...args) {
+  const loop = useRef()
+
+  // Remember the latest callback.
+  useEffect(() => loop.current = () => fn(...args), [fn, args])
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      loop.current()
+    }
+    if (delay !== null && delay !== undefined) {
+      const id = setInterval(tick, delay)
+      return () => clearInterval(id)
+    }
+  }, [delay])
+}
